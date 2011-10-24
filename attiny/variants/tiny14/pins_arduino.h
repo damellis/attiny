a4ef5b0c47ca052631f2f9c2a6c0f8b1d61088e4
David A. Mellis <d.mellis@arduino.cc>
2011-10-24 16:06:50 -0400

    Renumbering pins for ATtiny24/44/84.

--- attiny/variants/tiny14/pins_arduino.h
+++ attiny/variants/tiny14/pins_arduino.h
@@ -34,18 +34,27 @@
 //
 //                           +-\/-+
 //                     VCC  1|    |14  GND
-//             (D  0)  PB0  2|    |13  AREF (D 10)
-//             (D  1)  PB1  3|    |12  PA1  (D  9) 
-//                     PB3  4|    |11  PA2  (D  8) 
-//  PWM  INT0  (D  2)  PB2  5|    |10  PA3  (D  7) 
-//  PWM        (D  3)  PA7  6|    |9   PA4  (D  6) 
-//  PWM        (D  4)  PA6  7|    |8   PA5  (D  5)        PWM
+//             (D 10)  PB0  2|    |13  AREF (D  0)
+//             (D  9)  PB1  3|    |12  PA1  (D  1) 
+//                     PB3  4|    |11  PA2  (D  2) 
+//  PWM  INT0  (D  8)  PB2  5|    |10  PA3  (D  3) 
+//  PWM        (D  7)  PA7  6|    |9   PA4  (D  4) 
+//  PWM        (D  6)  PA6  7|    |8   PA5  (D  5)        PWM
 //                           +----+
 
+const static uint8_t A0 = 0;
+const static uint8_t A1 = 1;
+const static uint8_t A2 = 2;
+const static uint8_t A3 = 3;
+const static uint8_t A4 = 4;
+const static uint8_t A5 = 5;
+const static uint8_t A6 = 6;
+const static uint8_t A7 = 7;
+
 #define digitalPinToPCICR(p)    ( ((p) >= 0 && (p) <= 10) ? (&GIMSK) : ((uint8_t *)0) )
-#define digitalPinToPCICRbit(p) ( ((p) <= 2) ? PCIE1 : PCIE0 )
-#define digitalPinToPCMSK(p)    ( ((p) <= 2) ? (&PCMSK1) : (((p) <= 10) ? (&PCMSK0) : ((uint8_t *)0)) )
-#define digitalPinToPCMSKbit(p) ( ((p) <= 2) ? (p) : (10 - (p)) )
+#define digitalPinToPCICRbit(p) ( ((p) <= 7) ? PCIE0 : PCIE1 )
+#define digitalPinToPCMSK(p)    ( ((p) <= 7) ? (&PCMSK0) : (((p) <= 10) ? (&PCMSK1) : ((uint8_t *)0)) )
+#define digitalPinToPCMSKbit(p) ( ((p) <= 7) ? (p) : (10 - (p)) )
 
 #ifdef ARDUINO_MAIN
 
@@ -75,30 +84,30 @@ const uint16_t PROGMEM port_to_input_PGM[] =
 
 const uint8_t PROGMEM digital_pin_to_port_PGM[] = 
 {
-  PB, /* 0 */
+  PA, /* 0 */
+  PA,
+  PA,
+  PA,
+  PA,
+  PA,
+  PA,
+  PA,
+  PB, /* 8 */
   PB,
   PB,
-  PA,
-  PA,
-  PA,
-  PA,
-  PA,
-  PA, /* 8 */
-  PA,
-  PA,
 };
 
 const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] = 
 {
-  _BV(0), /* 0, port B */
+  _BV(0), /* port A */
   _BV(1),
   _BV(2),
-  _BV(7), /* 3 port B */
-  _BV(6),
-  _BV(5),
-  _BV(4),
   _BV(3),
-  _BV(2), 
+  _BV(4),
+  _BV(5), 
+  _BV(6),
+  _BV(7),
+  _BV(2), /* port B */
   _BV(1),
   _BV(0),
 };
@@ -107,14 +116,13 @@ const uint8_t PROGMEM digital_pin_to_timer_PGM[] =
 {
   NOT_ON_TIMER,
   NOT_ON_TIMER,
-  TIMER0A, /* OC0A */
-  TIMER0B, /* OC0B */
-  TIMER1A, /* OC1A */
-  TIMER1B, /* OC1B */
-  NOT_ON_TIMER,
   NOT_ON_TIMER,
   NOT_ON_TIMER, 
   NOT_ON_TIMER,
+  TIMER1B, /* OC1B */
+  TIMER1A, /* OC1A */
+  TIMER0B, /* OC0B */
+  TIMER0A, /* OC0A */
   NOT_ON_TIMER,
   NOT_ON_TIMER,
 };
