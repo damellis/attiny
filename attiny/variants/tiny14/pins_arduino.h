8efafe219dc74b815362b1028218e00266a745fe
Scott Daniels <sdmail@provideyourown.com>
2013-02-10 13:34:18 -0600

    Added analog & reset functions to pinout diagram

--- attiny/variants/tiny14/pins_arduino.h
+++ attiny/variants/tiny14/pins_arduino.h
@@ -34,13 +34,14 @@
 //
 //                           +-\/-+
 //                     VCC  1|    |14  GND
-//             (D 10)  PB0  2|    |13  AREF (D  0)
-//             (D  9)  PB1  3|    |12  PA1  (D  1) 
-//                     PB3  4|    |11  PA2  (D  2) 
-//  PWM  INT0  (D  8)  PB2  5|    |10  PA3  (D  3) 
-//  PWM        (D  7)  PA7  6|    |9   PA4  (D  4) 
-//  PWM        (D  6)  PA6  7|    |8   PA5  (D  5)        PWM
+//             (D 10)  PB0  2|    |13  AREF (D  0) (A0/AREF)
+//             (D  9)  PB1  3|    |12  PA1  (D  1) (A1)
+//             RESET   PB3  4|    |11  PA2  (D  2) (A2)
+//  PWM  INT0  (D  8)  PB2  5|    |10  PA3  (D  3) (A3)
+//  PWM    (A7)(D  7)  PA7  6|    |9   PA4  (D  4) (A4)
+//  PWM    (A6)(D  6)  PA6  7|    |8   PA5  (D  5) (A5)  PWM
 //                           +----+
+
 
 const static uint8_t A0 = 0;
 const static uint8_t A1 = 1;
